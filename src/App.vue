b5eecc0518cc56918e0a2ec4babaac2eb49bafd3
giang.vu <giang.vu@impl.vn>
2021-05-05 18:14:41 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld v-show="false" />
+  <ThemeToggle />
+  <Neummorphic />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import ThemeToggle from "./components/ThemeToggle.vue";
+import Neummorphic from "./components/Neummorphic.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+    ThemeToggle,
+    Neummorphic,
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "../src/assets/app.scss";
 </style>
---
+++ src/components/Neummorphic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="neumorphic">
+    <div class="rings">
+      <div class="ring --first"></div>
+      <div class="ring --second"></div>
+      <div class="ring --third"></div>
+      <div class="ring --fourth"></div>
+      <!-- <div class="ring --fifth"></div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
